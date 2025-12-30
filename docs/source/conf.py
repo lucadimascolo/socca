52bbc9a53a604fc9a8f7fbdc6f2529ea3d9940ef
Luca Di Mascolo <lucadimascolo@gmail.com>
2025-12-30 16:42:11 +0100

    docs: remove unused html theme options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,6 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
@@ -35,43 +33,6 @@ html_theme = "furo"
 
 html_static_path = ['_static']
 html_css_files = ['custom.css']
-
-# html_theme_options = {
-#   "accent_color": "tomato",
-# }
-
-'''
-html_theme_options = {
-    "nav_links_align": "right",
-    "nav_links": [
-        {
-            "title": "Wanna help?",
-            "url": "wannahelp",
-            "children": [
-                {
-                    "title": "Report an issue",
-                    "url": ISSUE_URL.format("bug_report"),
-                    "summary": "Found a bug? Let us know!",
-                    "external": True,
-                },
-                {
-                    "title": "Request a feature",
-                    "url": ISSUE_URL.format("feature_request"),
-                    "summary": "Would you like to see a new feature?",
-                    "external": True,
-                },
-                {
-                    "title": "Contribute",
-                    "url": "https://github.com/lucadimascolo/socca/pulls",
-                    "summary": "Submit a pull request",
-                    "external": True,
-                },
-
-            ],
-        },
-    ]
-}
-'''
 
 extensions = [
     "myst_parser",
