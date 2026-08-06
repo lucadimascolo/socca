505f90de11dcb65a514329b2966b59f3a87f57ad
stefaneduard1 <stefaneduard1@gmail.com>
2026-08-06 17:51:25 +0200

    feat: add BoundTo for tying parameters into namespaced sub-components
    Replace boundto()'s lambda-based return value with a BoundTo(key)
    marker object, and have boundto() search a component's _namespaces
    (e.g. Disk.radial/vertical, Bar.radial/geometry) to resolve the
    correct namespaced key when the target parameter lives on a
    sub-component rather than the top-level component. This lets a Bar's
    geometry.inc/geometry.theta be tied to a Disk's inc/theta.
    
    socca.models.Model.addcomponent/getmap/getmodel are updated to
    recognize BoundTo alongside lambdas/functions when detecting and
    resolving tied parameters. Disk now also exposes a _namespaces dict
    for radial/vertical, matching Bar's radial/geometry.
    
    Note: BoundTo is not callable and isn't yet recognized by the
    .parameters() display methods on Component/Disk/Bar/Model (which
    still only check for lambda/function types) -- calling .parameters()
    on a component with a BoundTo-tied parameter will raise. Flagged for
    follow-up.
    
    (cherry picked from commit 0ef134475b8fb0b1feb37fd93a7d05f6f13aeb35)

--- socca/models/__init__.py
+++ socca/models/__init__.py
@@ -11,6 +11,7 @@ import numpyro.distributions
 
 from socca.units import conversion_factor as _cfactor
 
+from ..priors import BoundTo
 from . import config
 from .base import Component
 from .misc import Point, Background
@@ -305,7 +306,7 @@ class Model:
             if isinstance(par, numpyro.distributions.Distribution):
                 self.paridx.append(len(self.params) - 1)
 
-            if isinstance(par, (types.LambdaType, types.FunctionType)):
+            if isinstance(par, (types.LambdaType, types.FunctionType, BoundTo)):
                 self.tied.append(True)
             else:
                 self.tied.append(False)
@@ -467,12 +468,18 @@ class Model:
 
         for ki, key in enumerate(self.params):
             if self.tied[ki]:
-                kwarg = list(
-                    inspect.signature(self.priors[key]).parameters.keys()
-                )
-                kwarg = {k: pars[k] for k in kwarg}
-                pars[key] = self.priors[key](**kwarg)
-                del kwarg
+                print(key)
+                print(self.priors[key])
+                print(self.priors[key].key)
+                if isinstance(self.priors[key], BoundTo):
+                    pars[key] = pars[self.priors[key].key]
+                else:
+                    kwarg = list(
+                        inspect.signature(self.priors[key]).parameters.keys()
+                    )
+                    kwarg = {k: pars[k] for k in kwarg}
+                    pars[key] = self.priors[key](**kwarg)
+                    del kwarg
 
         mraw, msmo, mbkg, _ = self.getmodel(
             img, pars, doresp=False, doexp=False
@@ -631,12 +638,15 @@ class Model:
 
         for ki, key in enumerate(self.params):
             if self.tied[ki]:
-                kwarg = list(
-                    inspect.signature(self.priors[key]).parameters.keys()
-                )
-                kwarg = {k: pars[k] for k in kwarg}
-                pars[key] = self.priors[key](**kwarg)
-                del kwarg
+                if isinstance(self.priors[key], BoundTo):
+                    pars[key] = pars[self.priors[key].key]
+                else:
+                    kwarg = list(
+                        inspect.signature(self.priors[key]).parameters.keys()
+                    )
+                    kwarg = {k: pars[k] for k in kwarg}
+                    pars[key] = self.priors[key](**kwarg)
+                    del kwarg
 
         mbkg = jp.zeros(img.data.shape)
         mraw = jp.zeros(img.data.shape)
